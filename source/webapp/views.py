de78776d9b1556576dcef57944e783b4dc1572f8
talgar <otalgarbek@gmail.com>
2020-07-23 19:46:57 +0600

    добавленj перенапрвление

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from webapp.models import Article
+from django.http import HttpResponseNotAllowed
 
 def index_view(request):
     data = Article.objects.all()
@@ -13,9 +14,19 @@ def article_create_view(request):
     if request.method == 'GET':
         return render(request, 'article_create.html')
     elif request.method == 'POST':
-        context = {
-            'title': request.POST.get('title'),
-            'content': request.POST.get('content'),
-            'author': request.POST.get('author')
-        }
-        return render(request, 'article_view.html', context)
+        title = request.POST.get('title')
+        text = request.POST.get('content')
+        author = request.POST.get('author')
+        article = Article.objects.create(title=title, text=text, author=author)
+        return redirect(f'/article/?pk={article.pk}')
+    else:
+        HttpResponseNotAllowed(permitted_methods=['GET','POST'])
+
+
+
+
+def article_view(request):
+    article_id = request.GET.get('pk')
+    article = Article.objects.get(pk=article_id)
+    context = {'article': article}
+    return render(request, 'article_view.html', context)

efd221a11efedc272011fc930f5b98eb79d90aee
talgar <otalgarbek@gmail.com>
2020-07-29 15:36:36 +0600

    добавлено обновление статей

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -33,3 +33,18 @@ def article_view(request, pk):
     article = Article.objects.get(pk=pk)
     context = {'article': article}
     return render(request, 'article_view.html', context)
+
+def article_update_view(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    if request.method == 'GET':
+        return render(request, 'article_update.html', context={'article':article})
+    elif request.method == 'POST':
+        article.title = request.POST.get('title')
+        article.text = request.POST.get('content')
+        article.author = request.POST.get('author')
+        article.save()
+
+        return redirect('article_view', pk=article.pk
+                        )
+    else:
+        HttpResponseNotAllowed(permitted_methods=['GET','POST'])
